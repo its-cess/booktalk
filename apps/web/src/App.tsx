56c60387c3c4c378d2a7f0d99926bece897747bd
Cecily Toro <cecily.toro@postman.com>
2026-02-07 15:35:04 -0500

    feat(web): inital frontend setup wiwth Vite, Tailwind, shadcn/ui

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from "./components/components/ui/button";
+
+export default function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-blue-100">
+      <h1 className="text-4xl font-bold text-blue-800">BookTalk!</h1>
+      <Button>Click me</Button>
+    </div>
+  );
+}
